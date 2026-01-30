acf29f05682f723b104bdfe2630b9186787c584d
thomasleroy <e254298l@etu.univ-nantes.fr>
2026-01-30 11:09:29 +0100

    add some boxes

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,11 +9,11 @@ export default define.page(function Home() {
         <title>Thomas Leroy</title>
       </Head>
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <h1 class="text-4xl font-bold">À propos de moi</h1>
+        <h1 class="text-4xl font-bold">Bienvenue sur mon portfolio.</h1>
         <p class="my-4">
-          Bienvenue sur mon portfolio.
+          Je suis Thomas Leroy, un étudiant en BUT Informatique à l'IUT de
+          Nantes.
         </p>
-        <LinkCV />
       </div>
       {/* Block 1: Contact */}
       <div class="flex flex-col items-center justify-center p-8 bg-white text-gray-900">
@@ -21,22 +21,35 @@ export default define.page(function Home() {
         <p>TODO</p>
       </div>
 
-      {/* Block 2: Projets */}
-      <div class="flex flex-col items-center justify-center p-8 bg-gray-100 text-gray-900">
+      {/* Block 2: Projets Académiques */}
+      <div class="flex flex-col items-center justify-center p-8 bg-white text-gray-900">
+        <h2 class="text-3xl font-bold mb-4">Projets Académiques</h2>
+        <p>TODO</p>
+      </div>
+
+      {/* Block 3: Projets Professionnels */}
+      <div class="flex flex-col items-center justify-center p-8 bg-white text-gray-900">
+        <h2 class="text-3xl font-bold mb-4">Projets Professionnels</h2>
+        <p>TODO</p>
+      </div>
+
+      {/* Block 4: Projets Personnels */}
+      <div class="flex flex-col items-center justify-center p-8 bg-white text-gray-900">
         <h2 class="text-3xl font-bold mb-4">Projets Personnels</h2>
         <p>TODO</p>
       </div>
 
-      {/* Block 3: Compétences / Formation */}
+      {/* Block 5: Compétences / Formation */}
       <div class="flex flex-col items-center justify-center p-8 bg-gray-50 text-gray-900">
         <h2 class="text-3xl font-bold mb-4">Compétences</h2>
         <p>TODO</p>
       </div>
 
-      {/* Block 4: Contact */}
+      {/* Block 6: Contact */}
       <div class="flex flex-col items-center justify-center p-8 bg-white text-gray-900">
         <h2 class="text-3xl font-bold mb-4">Contact</h2>
         <p>Mail : thomasleroycontact25@gmail.com</p>
+        <LinkCV />
       </div>
     </div>
   );
