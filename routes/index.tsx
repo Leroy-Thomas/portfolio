89dcb389b3921225a33d81ddfa98f29ba8f8d6f6
thomasleroy <e254298l@etu.univ-nantes.fr>
2026-01-30 10:53:08 +0100

    add boxes

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,6 @@ import { define } from "../utils.ts";
 import LinkCV from "../islands/button_link_cv.tsx";
 
 export default define.page(function Home() {
-
   return (
     <div class="px-4 py-8 mx-auto bg_white min-h-screen">
       <Head>
@@ -15,6 +14,29 @@ export default define.page(function Home() {
           Bienvenue sur mon portfolio.
         </p>
         <LinkCV />
+      </div>
+      {/* Block 1: Contact */}
+      <div class="flex flex-col items-center justify-center p-8 bg-white text-gray-900">
+        <h2 class="text-3xl font-bold mb-4">Projets Professionnels</h2>
+        <p>TODO</p>
+      </div>
+
+      {/* Block 2: Projets */}
+      <div class="flex flex-col items-center justify-center p-8 bg-gray-100 text-gray-900">
+        <h2 class="text-3xl font-bold mb-4">Projets Personnels</h2>
+        <p>TODO</p>
+      </div>
+
+      {/* Block 3: Compétences / Formation */}
+      <div class="flex flex-col items-center justify-center p-8 bg-gray-50 text-gray-900">
+        <h2 class="text-3xl font-bold mb-4">Compétences</h2>
+        <p>TODO</p>
+      </div>
+
+      {/* Block 4: Contact */}
+      <div class="flex flex-col items-center justify-center p-8 bg-white text-gray-900">
+        <h2 class="text-3xl font-bold mb-4">Contact</h2>
+        <p>Mail : thomasleroycontact25@gmail.com</p>
       </div>
     </div>
   );
